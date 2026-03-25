967680b5d11506388bad4150fccf3c78ca7d968d
wishes-hyundo <wishes@wishes.co.kr>
2026-03-25 09:51:46 +0900

    feat: add CompareBar to ConditionalLayout

--- src/components/ConditionalLayout.tsx
+++ src/components/ConditionalLayout.tsx
@@ -21,7 +21,8 @@ export function ConditionalLayout({ children }: { children: React.ReactNode }) {
       <main className="flex-1">{children}</main>
       <Footer />
       <FloatingButtons />
-      <AIChatBot />
+      <CompareBar />
+          <AIChatBot />
     </FavoritesProvider>
   );
 }
