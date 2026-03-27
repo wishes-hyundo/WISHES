a3336b65ef04f86a25cf7b2c6bfa0f76d6a11a59
wishes-hyundo <wishes@wishes.co.kr>
2026-03-27 13:53:29 +0900

    fix: update ImageGallery component with clean version using regular img tags

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -173,7 +173,7 @@ export default function ImageGallery({ images, title, deal, status, dealColor, s
             </>
           )}
 
-          {/* 하단 썸네일 */}
+          {/* 하단 썴네일 */}
           {images.length > 1 && (
             <div className="absolute bottom-4 left-1/2 -translate-x-1/2 flex gap-2 px-4 py-2 bg-black/50 rounded-full max-w-[90vw] overflow-x-auto">
               {images.map((img, idx) => (
