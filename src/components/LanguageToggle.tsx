ea5f172a776eaaf138b0d26277c918962c13b735
wishes-hyundo <wishes@wishes.co.kr>
2026-04-01 16:38:15 +0900

    fix: add missing LanguageToggle stub

---
+++ src/components/LanguageToggle.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react';
+
+export function LanguageProvider({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
+
+export function LanguageToggle() {
+  return null;
+}
+
+export default LanguageToggle;
